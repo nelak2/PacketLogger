bc6ca91f9acd84ad936cdc15ad9a84685d17277b
orizvi <hello468@hotmail.com>
2013-07-07 20:10:51 -0600

    Corrected error with namespaces

--- PacketSender/BNERMessage.cs
+++ PacketSender/BNERMessage.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Text.ASCIIEncoding;
-using System.Text.Encoding;
 
 namespace PacketSender
 {
